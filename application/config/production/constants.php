09777ffe45a1ea8ad91110f3ebaa34470bea70c0
Ivan Palomino <ivanp_151@hotmail.com>
2012-12-17 17:49:15 -0500

    contantes v3

--- application/config/production/constants.php
+++ application/config/production/constants.php
@@ -3,4 +3,4 @@
 include_once dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.basename(__FILE__);
 
 define('MEDIAURL',			'http://colegio.phpfogapp.com/assets/');
-define('STATICVERSION',		2);
+define('STATICVERSION',		3);
